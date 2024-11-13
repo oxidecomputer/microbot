74d2b5e67eba1dde8bb4a071e248e2cb32e97a82
Augustus Mayo <gusmayo@gmail.com>
2024-11-13 12:15:18 -0600

    Add licenses

--- tests/bot_conversation.rs
+++ tests/bot_conversation.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use matrix_chat_bot::{CommandArgs, MatrixBot};
 use matrix_chat_bot_test_utils::{setup, spawn_bot};
 use rand::{distributions::Alphanumeric, Rng};
